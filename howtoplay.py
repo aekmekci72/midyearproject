365b7e2854f428e2e12d399be37e77543f1b5869
aekmekci72 <anna.ekmekci@gmail.com>
2023-02-01 11:14:23 -0500

    home screen --> how to play --> quit

--- howtoplay.py
+++ howtoplay.py
@@ -16,15 +16,15 @@ width = screen.get_width()
 height = screen.get_height()
 smallfont = pygame.font.SysFont('Ariel',35)
 text = smallfont.render('S T A R T' , True , color)
+
+
+splash_page = pygame.image.load('howtoplayback.png')
 continueb = pygame.image.load('continue.png')
-
-
-splash_page = pygame.image.load('ship_sink.jpeg')
-splash_water = pygame.image.load('water_drop.png')
+#splash_water = pygame.image.load('water_drop.png')
 
 scaled_splash = pygame.transform.scale(splash_page, (width/2, height/2))
-scaled_water = pygame.transform.scale(splash_water, (width/2, height/4))
-splash_water1 = pygame.transform.flip(scaled_water, True, False)
+#scaled_water = pygame.transform.scale(splash_water, (width/2, height/4))
+#splash_water1 = pygame.transform.flip(scaled_water, True, False)
 
 
 def blit_alpha(target, source, location, opacity):
@@ -36,8 +36,8 @@ def blit_alpha(target, source, location, opacity):
     temp.set_alpha(opacity)        
     target.blit(temp, location)
 
-text_splash = font1.render('Sick Or Swim', False, 'white')
-text_splash_name = font.render('By: Shreeja And Anna', False, 'white')
+text_splash = font1.render('How to Play', False, 'white')
+text_splash_name = font.render('this is the game description ig', False, 'white')
 
 play_button = pygame.image.load('play_button.png').convert_alpha()
 
@@ -63,20 +63,21 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             (width/2-500,height/2+200)
             if width/2-500 <= pygame.mouse.get_pos()[0] <= width/2-360 and height/2+200 <= pygame.mouse.get_pos()[1] <= height/2+400:
-                os.system("howtoplay.py 1")
+                os.system("screen2.py 1")
                 pygame.quit()
         
     screen.blit(text , (width/2+50,height/2))
     position=pygame.mouse.get_pos()
     screen.blit(scaled_splash,(0,0))
-    blit_alpha(screen, scaled_water,(0,0),150)
-    blit_alpha(screen, splash_water1,(0,450),150)
+   # blit_alpha(screen, scaled_water,(0,0),150)
+   # blit_alpha(screen, splash_water1,(0,450),150)
     screen.blit(text_splash, (200,70))
-    screen.blit(text_splash_name, (165,570))
-    scaled_splash = pygame.transform.smoothscale(scaled_splash, (width, height)) 
-    scaled_water = pygame.transform.smoothscale(scaled_water, (width, height)) 
     screen.blit(continueb, (width/2-500,height/2+200))
     continueb = pygame.transform.smoothscale(continueb, (140, 200)) 
+    screen.blit(text_splash_name, (165,270))
+    #scaled_water = pygame.transform.scale(splash_water, (width/2, height/4))
+    scaled_splash = pygame.transform.smoothscale(scaled_splash, (width, height)) 
+    
          
     pygame.display.update()
     clock.tick(60)
@@ -84,3 +85,26 @@ while True:
 
 
 
+
+while True:
+    for ev in pygame.event.get():
+          
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+              
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            if width/2 <= pygame.mouse.get_pos()[0] <= width/2+240 and height/2 <= pygame.mouse.get_pos()[1] <= height/2+40:
+                os.system("screen2.py 1")
+                pygame.quit()
+                  
+    screen.fill((0,0,0))
+      
+    if width/2 <= pygame.mouse.get_pos()[0] <= width/2+140 and height/2 <= pygame.mouse.get_pos()[1] <= height/2+40:
+        pygame.draw.rect(screen,color_light,[width/2,height/2,240,40])
+          
+    else:
+        pygame.draw.rect(screen,color_dark,[width/2,height/2,240,40])
+      
+    screen.blit(text , (width/2+50,height/2))
+      
+    pygame.display.update()
