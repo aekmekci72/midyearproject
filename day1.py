0d89a9a4f8fc0146ecda77d4541c4a272676d4d0
aekmekci72 <anna.ekmekci@gmail.com>
2023-02-05 10:50:36 -0500

    connecting

--- day1.py
+++ day1.py
@@ -48,7 +48,7 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             (width/2-500,height/2+200)
             if width/2-500 <= pygame.mouse.get_pos()[0] <= width/2-360 and height/2+200 <= pygame.mouse.get_pos()[1] <= height/2+400:
-                os.system("screen4.py 1")
+                os.system("ship.py 1")
                 pygame.quit()
         
     screen.blit(text , (0,0))
