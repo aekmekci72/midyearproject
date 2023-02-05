214fea6019955bf6b5c4a1ed4e641b9913f18023
aekmekci72 <anna.ekmekci@gmail.com>
2023-02-04 23:24:57 -0500

    basic basic basic intro (add images! - will ask anaya later hahaha)

--- screen2.py
+++ screen2.py
@@ -1,34 +1,78 @@
-import pygame, os
+import pygame
+import os
 
 pygame.init()
-screen= pygame.display.set_mode((800, 800))
+screen= pygame.display.set_mode()
+pygame.display.set_caption('Sick or Swim')
+
+clock = pygame.time.Clock()
+font = pygame.font.Font('Neucha-Regular.ttf',60)
+font1 = pygame.font.Font('PermanentMarker-Regular.ttf', 30)
+
 color = (255,255,255)
 color_light = (170,170,170)
 color_dark = (100,100,100)
 width = screen.get_width()
 height = screen.get_height()
-smallfont = pygame.font.SysFont('Ariel',35)
-text = smallfont.render('quit' , True , color)
-  
+smallfont = pygame.font.SysFont('Arial',35)
+text = smallfont.render('S T A R T' , True , color)
+continueb = pygame.image.load('continue.png')
+
+
+splash_page = pygame.image.load('standinimage.png')
+
+scaled_splash = pygame.transform.scale(splash_page, (800, 800))
+
+
+def blit_alpha(target, source, location, opacity):
+    x = location[0]
+    y = location[1]
+    temp = pygame.Surface((source.get_width(), source.get_height())).convert()
+    temp.blit(target, (-x, -y))
+    temp.blit(source, (0, 0))
+    temp.set_alpha(opacity)        
+    target.blit(temp, location)
+
+text_splash = font1.render('You are but a lowly human in the small town of [TOWN NAME].', False, 'white')
+text_splash1 = font1.render('Fortunately, you are lucky enough to have a good job, a wonderful wife, and two children.', False, 'white')
+counter=1
+
+
 while True:
-    for ev in pygame.event.get():
-          
-        if ev.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
-              
-        if ev.type == pygame.MOUSEBUTTONDOWN:
-            if width/2 <= pygame.mouse.get_pos()[0] <= width/2+140 and height/2 <= pygame.mouse.get_pos()[1] <= height/2+40:
+            exit()
         
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            counter+=1
+            if counter==2:
+                text_splash = font1.render("You have everything a person could ever want in life", False, "white")
+                text_splash1 = font1.render("However, one day, everything changes...", False, "white")
+            if counter==3:
+                text_splash = font1.render("A disease called the [DISEASE NAME] washes through the town", False, "white")
+                text_splash1 = font1.render("One day, when you come home...", False, "white")
+            if counter==4:
+                text_splash = font1.render("You find your wife sick", False, "white")
+                text_splash1 = font1.render("Fast forward a few weeks...", False, "white")
+            if counter==5:
+                text_splash = font1.render("You are all alone. [INSERT DRAWING OF GRAVEYARD]", False, "white")
+                text_splash1 = font1.render("There is only one thing left to do...", False, "white")
+            if counter==6:
+                text_splash = font1.render("You embark on a ship called [SHIP NAME]", False, "white")
+                text_splash1 = font1.render("Thus, your adventure begins...", False, "white")
+            if counter==7:
+                os.system("day1.py 1")
                 pygame.quit()
-                  
-    screen.fill((110,110,110))
-      
-    if width/2 <= pygame.mouse.get_pos()[0] <= width/2+140 and height/2 <= pygame.mouse.get_pos()[1] <= height/2+40:
-        pygame.draw.rect(screen,color_light,[width/2,height/2,240,40])
-          
-    else:
-        pygame.draw.rect(screen,color_dark,[width/2,height/2,240,40])
-      
-    screen.blit(text,(width/2+50,height/2))
-      
+
+        
+    screen.blit(text , (0,0))
+    position=pygame.mouse.get_pos()
+    screen.blit(scaled_splash,(0,0))
+    screen.blit(text_splash, (60,70))
+    screen.blit(text_splash1, (60,140))
+
+    scaled_splash = pygame.transform.smoothscale(scaled_splash, (width, height)) 
+         
     pygame.display.update()
+    clock.tick(60)
