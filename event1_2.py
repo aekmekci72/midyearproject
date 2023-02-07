ff021aafa341ebb8014e8deda5d831361d8bdd42
aekmekci72 <anna.ekmekci@gmail.com>
2023-02-07 11:17:22 -0500

    added day 1 event 2

---
+++ event1_2.py
@@ -0,0 +1,65 @@
+import pygame
+import os
+import time
+
+pygame.init()
+screen= pygame.display.set_mode()
+pygame.display.set_caption('Sick or Swim')
+
+clock = pygame.time.Clock()
+font = pygame.font.Font('Neucha-Regular.ttf',60)
+font1 = pygame.font.Font('PermanentMarker-Regular.ttf', 20)
+
+color = (255,255,255)
+color_light = (170,170,170)
+color_dark = (100,100,100)
+width = screen.get_width()
+height = screen.get_height()
+smallfont = pygame.font.SysFont('Arial',35)
+
+
+splash_page = pygame.image.load('event1_1_2.png')
+scaled_splash = pygame.transform.smoothscale(splash_page, (width, height))
+
+
+
+def blit_alpha(target, source, location, opacity):
+    x = location[0]
+    y = location[1]
+    temp = pygame.Surface((source.get_width(), source.get_height())).convert()
+    temp.blit(target, (-x, -y))
+    temp.blit(source, (0, 0))
+    temp.set_alpha(opacity)        
+    target.blit(temp, location)
+
+health = 100
+wealth = 100
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                os.system("python event1_2mone.py 1")
+                pygame.quit()
+            elif event.key ==pygame.K_RIGHT:
+                os.system("python event1_2surv.py 1")
+                pygame.quit()
+                
+            
+            position=pygame.mouse.get_pos()
+            scaled_splash = pygame.transform.smoothscale(splash_page, (width, height))
+    
+        
+    pygame.display.update()
+        
+    text_splash=font1.render("", False, 'black')
+    screen.blit(text_splash, (60,70))
+    position=pygame.mouse.get_pos()
+    screen.blit(scaled_splash,(0,0))
+    scaled_splash = pygame.transform.smoothscale(scaled_splash, (width, height))
+         
--- ship1.py
+++ ship1.py
@@ -42,15 +42,16 @@ clock_game = 0
 global min, hour, count, var,label
 booleee=False
 file =open("infofile.txt")
+hour=0
 for line in file:
     line=line.strip()
     if line=="event1_1":
-        boleee=True
-
-if booleee==True:
-    hour=9
-    label=font1.render("You survived the choppy waters", False,"black")
-else:
+        hour=10
+        label=font1.render("You survived the choppy waters", False,"black")
+    if line=="event1_2":
+        hour=12
+        label=font1.render("You survived the dangerous waters", False,"black")
+if hour==0:
     hour=7
     label=font1.render("You wake up at 7 on your ship",False,"black")
 
@@ -68,6 +69,8 @@ def clockfunc():
     global min, hour, count, var
     if hour==9:
         os.system("python event1_1.py 1")
+    if hour==11:
+        os.system("python event1_2.py 1")
     time.sleep(0.1)
     min+=1
     if min<10:
