5a1de2840105a4244581d464bc889cccb3325b57
aekmekci72 <anna.ekmekci@gmail.com>
2023-02-19 00:19:34 -0500

    colordash + beginning of fight

--- day5/fight.py
+++ day5/fight.py
@@ -4,9 +4,9 @@ import pygame
 
 pygame.init()
 
-win_width = 600
-win_height = 400
-win = pygame.display.set_mode((win_width, win_height))
+screen_width = 600
+screen_height = 400
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Two Images and a Button")
 
@@ -24,12 +24,12 @@ text_width = text.get_width()
 text_height = text.get_height()
 
 image1_x = 50
-image1_y = (win_height - image_height) // 2
-image2_x = win_width - image_width - 50
-image2_y = (win_height - image_height) // 2
+image1_y = (screen_height - image_height) // 2
+image2_x = screen_width - image_width - 50
+image2_y = (screen_height - image_height) // 2
 
-button_x = (win_width - text_width) // 2
-button_y = (win_height - text_height) // 2
+button_x = (screen_width - text_width) // 2
+button_y = (screen_height - text_height) // 2
 
 running = True
 while running:
@@ -41,13 +41,13 @@ while running:
             if position[0]>button_x-10 and position[0]<(button_x-10+text_width+20) and position[1]>button_y-10 and position[1]<(button_y-10+text_height+20):
                 print("attacked")
 
-    win.fill((255, 255, 255))
+    screen.fill((255, 255, 255))
 
-    win.blit(image1, (image1_x, image1_y))
-    win.blit(image2, (image2_x, image2_y))
+    screen.blit(image1, (image1_x, image1_y))
+    screen.blit(image2, (image2_x, image2_y))
 
-    pygame.draw.rect(win, (0, 0, 255), (button_x - 10, button_y - 10, text_width + 20, text_height + 20))
-    win.blit(text, (button_x, button_y))
+    pygame.draw.rect(screen, (0, 0, 255), (button_x - 10, button_y - 10, text_width + 20, text_height + 20))
+    screen.blit(text, (button_x, button_y))
 
     pygame.display.update()
 
