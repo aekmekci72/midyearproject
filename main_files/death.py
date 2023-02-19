da52614726d1b4760ab755e072b40b35c9954d31
shreejalearn <shreeja.das.16@gmail.com>
2023-02-19 09:32:40 -0500

    replacing standin images

--- main_files/death.py
+++ main_files/death.py
@@ -7,7 +7,7 @@ pygame.display.set_caption('Sick or Swim')
 
 clock = pygame.time.Clock()
 font = pygame.font.Font('images_fonts/Neucha-Regular.ttf',60)
-font1 = pygame.font.Font('images_fonts/PermanentMarker-Regular.ttf', 30)
+font1 = pygame.font.Font('images_fonts/PermanentMarker-Regular.ttf', 90)
 
 color = (255,255,255)
 color_light = (170,170,170)
@@ -19,7 +19,7 @@ text = smallfont.render('S T A R T' , True , color)
 continueb = pygame.image.load('images_fonts/continue.png')
 
 
-splash_page = pygame.image.load('images_fonts/standinimage.png')
+splash_page = pygame.image.load('images_fonts/graveyard.jpg')
 
 scaled_splash = pygame.transform.scale(splash_page, (800, 800))
 
@@ -33,7 +33,7 @@ def blit_alpha(target, source, location, opacity):
     temp.set_alpha(opacity)        
     target.blit(temp, location)
 
-text_splash = font1.render('You died', False, 'white')
+text_splash = font1.render('You died...', False, 'white')
 text_splash1 = font1.render('Better luck next time!', False, 'white')
 counter=1
 
@@ -47,14 +47,14 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.quit()
 
-        
+    scaled_splash = pygame.transform.smoothscale(scaled_splash, (width, height)) 
+
     screen.blit(text , (0,0))
     position=pygame.mouse.get_pos()
     screen.blit(scaled_splash,(0,0))
     screen.blit(text_splash, (60,70))
-    screen.blit(text_splash1, (60,140))
+    screen.blit(text_splash1, (60,180))
 
-    scaled_splash = pygame.transform.smoothscale(scaled_splash, (width, height)) 
          
     pygame.display.update()
     clock.tick(60)
